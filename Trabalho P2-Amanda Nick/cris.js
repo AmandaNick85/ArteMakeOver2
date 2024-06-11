4bd6f6059e43d307e0f20369fef62e56a77a4f88
AmandaNick85 <167568532+AmandaNick85@users.noreply.github.com>
2024-06-10 22:55:05 -0300

    Add files via upload

---
+++ Trabalho P2-Amanda Nick/cris.js
@@ -0,0 +1,12 @@
+document.getElementById('instagrambutton').addEventListener('click', function() {
+    const urlParaCompartilhari = 'https://www.instagram.com/cp.arte_makeover';
+    window.open(urlParaCompartilhari, '_blank');
+});
+document.getElementById('youtubebutton').addEventListener('click', function() {
+    const urlParaCompartilhary = 'https://www.youtube.com/c/Cristianocabralpires?app=desktop';
+    window.open(urlParaCompartilhary, '_blank');
+});
+document.getElementById('wppbutton').addEventListener('click', function() {
+    const urlParaCompartilharw = 'https://wa.me/+5521982463923';
+    window.open(urlParaCompartilharw, '_blank');
+});
